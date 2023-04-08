2ac41ed872b18894db4ff370f7366b94cfd0dc24
Landon <landon.c.rivers@gmail.com>
2023-04-08 13:26:55 -0400

    Update import_bone_hierarchy.js

--- plugin/import_bone_hierarchy.js
+++ plugin/import_bone_hierarchy.js
@@ -3,7 +3,7 @@
 
     Plugin.register('import_bone_hierarchy', {
         name: 'Bone Hierarchy Importer',
-        icon: 'icon', // Replace 'icon' with an icon file or a Material Icons name
+        icon: 'import_export', // Replace 'icon' with an icon file or a Material Icons name
         author: 'h1ddentree productions',
         description: 'Import bone hierarchy from JSON files',
         version: '1.0.0',
@@ -11,10 +11,10 @@
         variant: 'both',
         onload() {
             importAction = new Action('import_bone_hierarchy', {
-                name: 'Import Bone Hierarchy from JSON',
-                description: 'Import bone hierarchy from a JSON file',
-                icon: 'bar_chart', // Replace 'icon' with an icon file or a Material Icons name
-                click: () => {
+            name: 'Import Bone Hierarchy from JSON',
+            description: 'Import bone hierarchy from a JSON file',
+            icon: 'import_export', // Replace 'icon' with an icon file or a Material Icons name
+            click: () => {
                     // Create file input element
                     const fileInput = document.createElement('input');
                     fileInput.type = 'file';
@@ -27,7 +27,7 @@
 
                         reader.onload = (event) => {
                             const boneHierarchy = JSON.parse(reader.result);
-                            const rootBone = this.createBlockbenchBone(boneHierarchy);
+                            const rootBone = createBlockbenchBone(boneHierarchy);
                             rootBone.addTo(Group.selected);
                         };
 
@@ -36,27 +36,31 @@
                     fileInput.click();
                 }
             });
-            MenuBar.addAction(importAction, 'tools.0');
+        MenuBar.addAction(importAction, 'tools.0');
         },
         onunload() {
             importAction.delete();
         },
-        createBlockbenchBone(boneNode) {
-            // Create a Blockbench bone from a bone node
-            const bbBone = new Group({
-                name: boneNode.name,
-                from: boneNode.position,
-                rotation: boneNode.rotation,
-                scale: boneNode.scale
-            });
-
-            // Process and add children
-            boneNode.children.forEach(child => {
-                const bbChild = this.createBlockbenchBone(child);
-                bbChild.addTo(bbBone);
-            });
-
-            return bbBone;
-        }
     });
 })();
+
+
+function createBlockbenchBone(boneNode) {
+    // Create a Blockbench bone from a bone node
+    const bbBone = new Group({
+        name: boneNode.name,
+        from: boneNode.position,
+        rotation: boneNode.rotation,
+        scale: boneNode.scale
+    });
+
+    // Process and add children
+    if (boneNode.children) {
+        boneNode.children.forEach(child => {
+            const bbChild = createBlockbenchBone(child);
+            bbChild.addTo(bbBone);
+        });
+    }
+
+    return bbBone;
+};
